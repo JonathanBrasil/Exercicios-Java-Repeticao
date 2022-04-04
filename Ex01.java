9f541bf9f6163ed863d9c6945727755f8c695367
JOnathanBrasil <j_jonatha_2011@hotmail.com>
2022-04-04 18:12:15 -0300

    add ex01

---
+++ Ex01.java
@@ -0,0 +1,19 @@
+package br.com.repeticao;
+
+public class Ex01 {
+//1-	Informar todos os números de 1000 a 1999 que quando divididos por 11 obtemos resto = 5. (FOR)
+	public static void main(String[] args) {
+		
+		for (int num = 1000; num <= 1999; num++)
+		{		
+			if(num % 11 == 5){
+				System.out.println("o núnero é: " + num);}
+		}
+}
+}
+			
+		
+
+
+
+
